a0906e43c45426ca45bb366d32ca46fe8419edc1
zzpwelkin <zhangzupeng19871203@126.com>
2013-12-27 16:42:40 +0800

    add directory tree for scraper

---
+++ celerytks/tasks.py
@@ -0,0 +1,26 @@
+import celery
+from scrapy.settings import CrawlerSettings
+from scrapy.crawler import CrawlerProcess
+
+# configure tasks by using celeryconfig module
+app = celery.Celery()
+app.config_from_object('celeryconfig')
+
+scrapy_settings = os.environ.get('OPENSHIFT_SCRAPY_SETTINGS')
+
+@app.task
+def addtest(x, y):
+    return x + y
+
+@app.task
+def run_spiders(name, **kwargs):
+    """
+    @param name: spider name
+    """
+    prs = CrawlerProcess( CrawlerSettings(scrapy_settings) )
+    crawler = prs.create_crawler()
+    for spdname, spd in crawler.spiders._spiders.iteritems():
+        if name == spdname:
+            spidercls = spd
+    crawler.crawl( spidercls(**kwargs) )
+    prs.start()
---
+++ wsgi/openshift/scraper/settings.py
@@ -0,0 +1,34 @@
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+import os,tmpfile
+
+ON_OPENSHIFT = False
+DJANGO_APP_NAME = 'openshift'
+TMP_DIR = '/tmp'
+IMAGES_STORE = os.path.josin( os.path.dirname(__file__), 'images')
+if os.environ.has_key('OPENSHIFT_REPO_DIR'):
+    ON_OPENSHIFT = True
+    TMP_DIR = os.environ['OPENSHIFT_TMP_DIR']
+    IMAGES_STORE = os.environ['OPENSHIFT_DATA_DIR']+'images/'
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{0}.settings".format(DJANGO_APP_NAME)) #Changed in DDS v.0.3
+
+SPIDER_MODULES = ['dynamic_scraper.spiders', 'openshift.scraper',]
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11' 
+
+
+ITEM_PIPELINES = [
+        'dynamic_scraper.pipelines.DjangoImagesPipeline',
+        'dynamic_scraper.pipelines.ValidationPipeline',
+        ]
+
+IMAGES_THUMBS = {
+        'small': (170, 170),
+        }
+
+LOG_FILE = tmpfile.mkstmp(dir=TMP_DIR, prefix=DJANGO_APP_NAME)
